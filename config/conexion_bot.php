e2413a4cacd6b3341c42ac77c79439f78f5db7ab
Iram Alvarez Rocha <iram.alvarezrch@gmail.com>
2022-05-17 01:01:13 -0500

    commit

--- config/conexion_bot.php
+++ config/conexion_bot.php
@@ -5,11 +5,7 @@ function conectar()
     $user="root";
     $pass="";
     $server="localhost";
-<<<<<<< HEAD
     $db="tzapata";
-=======
-    $db="zapatappdb_2";
->>>>>>> 8901b388c3c478edd8f9dd4f1a4c19c5432b1258
     $conexion=mysqli_connect($server, $user, $pass, $db) or die ("Error de conexion a la BD" . mysqli_connect_error());
     return $conexion;
 }
